0ea741790d9de5fc6c850e8a209ad4cb0ae81f8c
egor <egor.lentarev@yandex.ru>
2025-05-27 13:21:02 +1000

    Fix. Declaration

--- vue3-phone-input.d.ts
+++ vue3-phone-input.d.ts
@@ -4,8 +4,6 @@ declare module "vue3-telephone-input" {
     isValid: boolean | undefined;
   }
 
-  export { IVue3TelephoneInput };
-
   import Vue3TelephoneInput from "vue3-telephone-input/dist/vue3-telephone-input.es";
   export default Vue3TelephoneInput;
 }
