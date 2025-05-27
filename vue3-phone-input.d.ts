e312af132b87379569a8188a03fb39b25be8f1e5
egor <egor.lentarev@yandex.ru>
2025-05-27 13:12:24 +1000

    Fix. Module declaration. Interface declaration

--- vue3-phone-input.d.ts
+++ vue3-phone-input.d.ts
@@ -1,9 +1,9 @@
 declare module "vue3-telephone-input" {
+  export interface IVue3TelephoneInput {
+    phoneNumber: string;
+    isValid: boolean | undefined;
+  }
+
   import Vue3TelephoneInput from "vue3-telephone-input/dist/vue3-telephone-input.es";
   export default Vue3TelephoneInput;
-}
-
-declare interface IVue3TelephoneInput {
-  phoneNumber: string;
-  isValid: boolean | undefined;
 }
