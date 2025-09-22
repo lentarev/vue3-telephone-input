1ae5ac6a3614cf98847c023570d49b312b9c536e
egor <egor.lentarev@yandex.ru>
2025-09-22 15:43:20 +1000

    Fix type

--- vue3-phone-input.d.ts
+++ vue3-phone-input.d.ts
@@ -1,4 +1,6 @@
 declare module "vue3-telephone-input" {
   import Vue3TelephoneInput from "vue3-telephone-input/dist/vue3-telephone-input.es";
-  export default Vue3TelephoneInput;
+  import { IVue3TelephoneInput } from "./IVue3TelephoneInput";
+
+  export { Vue3TelephoneInput, IVue3TelephoneInput };
 }
